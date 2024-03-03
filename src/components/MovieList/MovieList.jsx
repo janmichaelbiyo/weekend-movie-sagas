42135b8e57c99aefc114f6f6ce37a2c61d45aca9
Jan Michael Biyo <janmichael.biyo@gmail.com>
2024-03-02 20:29:40 -0600

    got the movie poster to flip

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,25 +1,33 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import './MovieList.css';
 
 function MovieList() {
-
   const dispatch = useDispatch();
-  const movies = useSelector(store => store.movies);
+  const history = useHistory();
+  const movies = useSelector((store) => store.movies);
 
   useEffect(() => {
     dispatch({ type: 'FETCH_MOVIES' });
   }, []);
 
+  const submitDetails = (event) => {
+    event.preventDefault();
+    history.push('/details');
+  };
+
   return (
     <main>
       <h1>MovieList</h1>
       <section className="movies">
-        {movies.map(movie => {
+        {movies.map((movie) => {
           return (
-            <div data-testid='movieItem' key={movie.id}>
+            <div data-testid="movieItem" key={movie.id}>
               <h3>{movie.title}</h3>
-              <img src={movie.poster} alt={movie.title}/>
+              <div data-testid="toDetails" onClick={submitDetails}>
+                <img src={movie.poster} alt={movie.title} />
+              </div>
             </div>
           );
         })}
