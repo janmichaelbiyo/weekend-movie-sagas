fe489a2ffe265b09c2e4670a20c24de7b8d060e1
Jan Michael Biyo <janmichael.biyo@gmail.com>
2024-03-03 23:10:07 -0600

    got the styling done not the verybest but the most i can get and i like it

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import './MovieDetails.css';
 
 import IconButton from '@mui/material/IconButton';
 import UndoIcon from '@mui/icons-material/Undo';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Unstable_Grid2';
 
 function MovieDetails() {
   let history = useHistory();
@@ -15,12 +18,22 @@ function MovieDetails() {
   return (
     <>
       <div data-testid="movieDetails">
-        <h1>This is Details</h1>
-        <img src={movieDetail.poster} />
-        <p>
-          {movieDetail.title}, {movieDetail.description},{movieDetail.genre}
-        </p>
+        <h1 class="list">This is Details</h1>
 
+        <Grid container spacing={2}>
+          <Grid xs={8}>
+            <img src={movieDetail.poster} />
+          </Grid>
+          <Grid xs={6}>
+            <p class="detailname">{movieDetail.title}</p>
+          </Grid>
+          <Grid xs={7}>
+            <p class="detaildescrip"> {movieDetail.description}</p>
+          </Grid>
+          <Grid xs={4}>
+            <p class="detailgenre"> {movieDetail.genre}</p>
+          </Grid>
+        </Grid>
         <IconButton
           aria-label="back"
           size="large"
