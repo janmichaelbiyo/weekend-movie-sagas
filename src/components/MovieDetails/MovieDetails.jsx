82f5df3a0a3c89b9f44fbc6dc7626c36759d58b5
Jan Michael Biyo <janmichael.biyo@gmail.com>
2024-03-03 20:41:44 -0600

    finished all normal work will work on the styling now

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -3,16 +3,24 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
 function MovieDetails() {
+  let history = useHistory();
   const movieDetail = useSelector((store) => store.movieDetail);
+  const handleBacktoList = () => {
+    history.push('/');
+  };
 
   return (
     <>
-      <div>
+      <div data-testid="movieDetails">
         <h1>This is Details</h1>
         <img src={movieDetail.poster} />
         <p>
           {movieDetail.title}, {movieDetail.description},{movieDetail.genre}
         </p>
+
+        <button data-testid="toList" onClick={handleBacktoList}>
+          Back
+        </button>
       </div>
     </>
   );
